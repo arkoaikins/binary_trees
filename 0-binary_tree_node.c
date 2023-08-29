ae3ab41cb5e1ed6ed4dfc153b783bd497c051a61
arkoaikins <arkoaikins>
2023-08-29 19:54:10 +0000

    create funiton that creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - creates a binary tree node
  * @parent: points to the parent node of the node to create
  * @value: the value to put in the new node
- * 
+ *
  * Return: a pointer to the new node or NULL on failure
  *
  */
@@ -12,10 +12,10 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
-	
+
 	newnode = malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
-		return NULL;
+		return (NULL);
 
 	newnode->n = value;
 	newnode->parent = parent;
